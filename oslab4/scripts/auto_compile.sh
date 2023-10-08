d69ad5bced09b04a7643ccd61f30407d7d2578b4
PUPUHERO <victorwen17@gmail.com>
2023-10-08 20:48:31 +0800

    fix wd bug

--- oslab4/scripts/auto_compile.sh
+++ oslab4/scripts/auto_compile.sh
@@ -1,11 +1,10 @@
-cp /boot/config-`uname -r` linux-5.4.2/.config
-yes '' | make oldconfig
+cd "linux-5.4.2"
+sudo cp /boot/config-`uname -r` .config
+yes '' |sudo make oldconfig
 
-sed -i 's/debian\/canonical-certs\.pem//' linux-5.4.2/.config
+sed -i 's/debian\/canonical-certs\.pem//' .config
 
 sudo make -j$(nproc)
 sudo make modules_install -j 4
 sudo make install -j 4
 sudo update-grub
-sudo reboot
-
